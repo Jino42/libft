14888a8a6a1647af1a15476f1612acb5f9701f6c
Jino <ntoniolo@student.42.fr>
2018-07-06 13:32:54 +0200

    Add \n

--- basic/ft_error.c
+++ basic/ft_error.c
@@ -6,7 +6,7 @@
 /*   By: ntoniolo <ntoniolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/21 18:14:22 by ntoniolo          #+#    #+#             */
-/*   Updated: 2018/07/06 12:59:48 by ntoniolo         ###   ########.fr       */
+/*   Updated: 2018/07/06 13:31:37 by ntoniolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,10 @@
 bool	ft_error(char *string, bool (*func)(void *gen), void *gen)
 {
 	if (string)
+	{
 		ft_putstr(string);
+		ft_putchar('\n');
+	}
 	if (func)
 		return (func(gen));
 	return (false);
